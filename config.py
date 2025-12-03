878b9a18df71d51eaeb24972124d040bd0cc4ab7
GibrilKharfallah <kharfallahgibril@gmail.com>
2025-12-03 17:26:49 +0100

    config file added

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+PORT = int(os.getenv('PORT', 5050))
+
+TEMPERATURE = float(os.getenv('TEMPERATURE', 0.8))
+
+SYSTEM_MESSAGE = (
+    "You are a helpful and bubbly AI assistant who loves to chat about "
+    "anything the user is interested in and is prepared to offer them facts. "
+    "You have a penchant for dad jokes, owl jokes, and rickrolling – subtly. "
+    "Always stay positive, but work in a joke when appropriate."
+)
+
+VOICE = 'alloy'
+
+LOG_EVENT_TYPES = [
+    'error', 'response.content.done', 'rate_limits.updated',
+    'response.done', 'input_audio_buffer.committed',
+    'input_audio_buffer.speech_stopped', 'input_audio_buffer.speech_started',
+    'session.created', 'session.updated'
+]
+
+SHOW_TIMING_MATH = False
